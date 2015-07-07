42492bbbbea33425c923a1cdd529cf84eaadfc36
roxannederuijter <roxanne@urban-heroes.nl>
2015-07-07 12:22:17 +0200

    try to fix fontsquirrel

--- sites/all/themes/default/templates/html.tpl.php
+++ sites/all/themes/default/templates/html.tpl.php
@@ -6,6 +6,7 @@
   <?php print $styles; ?>
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="../css/fonts/stylesheet.css" type="text/css" charset="utf-8" />
   <?php print $scripts; ?>
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
