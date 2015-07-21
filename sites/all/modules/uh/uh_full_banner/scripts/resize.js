d94e0b952922e585870ee253d3bffeb00f08bb17
roxannederuijter <roxanne@urban-heroes.nl>
2015-07-21 17:02:53 +0200

    style

--- sites/all/modules/uh/uh_full_banner/scripts/resize.js
+++ sites/all/modules/uh/uh_full_banner/scripts/resize.js
@@ -4,12 +4,12 @@
             $(document).ready(function () {
                 //if(jQuery.fn.jquery < 1.6){return;}
                 // Define the target element(s)
-                var slides = $("#block-views-home-banner-block .field-name-field-banner img", context);
+                var slides = $("#block-views-front-banner-block .field-name-field-image img", context);
 
                 var listenImageresize = function(elm, image_width, image_height) {
                     var doc_width = $(window).width();
                     // can also use $(document).width(), which makes resizing faster
-                    var doc_height = $(window).height() - $(elm).offset().top; //($("#zone-branding").offset().top + $("#zone-branding").outerHeight()); // can also use $(document).height(), which makes resizing faster
+                    var doc_height = $(window).height() - $("#zone-header").offset().top;//$(elm).offset().top; //($("#zone-branding").offset().top + $("#zone-branding").outerHeight()); // can also use $(document).height(), which makes resizing faster
 
                     var image_ratio = image_width / image_height;
                     var new_width = doc_width;
@@ -20,12 +20,19 @@
                     $(elm).removeAttr('width').removeAttr('height');
                     if (new_height < doc_height) {
                         new_height = doc_height;
+
                         new_width = Math.round(new_height * image_ratio);
                         $(elm).width(new_width);
                         $(elm).height(new_height);
                         var width_offset = Math.round((new_width - doc_width) / 2);
                         $(elm).css("left", "-" + width_offset + "px");
                     }
+
+                    // Reposition tekst-div
+                    $(".views-field-field-front-banner").find(".field-name-field-tekst").css({
+                        'position': 'absolute',
+                        'bottom': '0'
+                    });
                 };
 
                 var getImage = function(img, callback){
